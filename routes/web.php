d85a5fd5a3f848dc904ef7bed68f2dd9379df73b
Toavina <sixlovatiana@gmail.com>
2026-07-20 09:11:17 +0300

    del

--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Http\Controllers\HomeController;
-
-Route::get('/', [HomeController::class, 'index']);
-Route::resource('home', HomeController::class);
---
+++ todo_list.md
@@ -0,0 +1,3 @@
+Version 1 :
+Code operateur 
+    .Configuration des prefixes valable de l'operateur (ex : 033)
